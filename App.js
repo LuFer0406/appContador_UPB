29ed06bbdc752c426f8346e04ad328680e17210b
LuisaFernanda <luisamoralesocp@gmail.com>
2022-11-18 21:36:42 -0500

    appContador v1

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function App() {
+
+  const [contador, setContador] = useState(0)
+  
+  const sumar = () => {
+    setContador(contador + 1);
+  }
+
+  const restar = () => {
+    setContador(contador - 1);
+  }
+
+  useEffect(() => {
+    if (contador >= 10) {
+      Alert.alert("Alerta", "No puedes seguir sumando...")
+    }
+  }, [contador])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style = {styles.title}>Contador: {contador}</Text>
+      
+      <TouchableOpacity style = {{...styles.button, right: 25, backgroundColor: "#bbf7d0"}} activeOpacity = {0.7} onPress = {() => sumar()} disabled = {contador >= 10}>
+        <Text style = {{...styles.nombres}}>
+        <Ionicons name="add-outline" size={22} color="green" />
+        Sumar
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style = {{...styles.button, left: 25, backgroundColor: "#fecdd3"}} activeOpacity = {0.7}  onPress = {() => restar()}>
+        <Text style = {{...styles.nombres}}>
+        <Ionicons name="remove-outline" size={22} color="red" />
+        Restar
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +44,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 30
+  },
+  button: {
+    alignItems: "center",
+    borderRadius: 30,
+    padding: 10,
+    position: "absolute",
+    bottom: 25,
+  },
+  nombres: {
+    fontSize: 18,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
